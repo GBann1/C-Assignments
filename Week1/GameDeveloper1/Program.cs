4465d06bad7f2b29853cf64d3ae28a42a2cfa68d
GBann <GTBannon@protonmail.com>
2023-08-28 16:21:59 -0600

    Finished GameDev1

--- Week1/GameDeveloper1/Program.cs
+++ Week1/GameDeveloper1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create instance of enemy
+Enemy Grunt = new Enemy("Grunt");
+
+// Three attacks
+Attack FlyingSwordFish = new Attack("Flying Sword Fish", 20);
+Attack ShushStick = new Attack("ShushStick", 8);
+Attack ElectricBeyblade = new Attack("Electric Beyblade", 13);
+
+Grunt.AttackList.Add(FlyingSwordFish);
+Grunt.AttackList.Add(ShushStick);
+Grunt.AttackList.Add(ElectricBeyblade);
+
+Console.WriteLine(Grunt.RandomAttack().Name);
