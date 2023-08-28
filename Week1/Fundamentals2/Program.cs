427c549c5a76ec2f4e2613ebca4328cd0fcb6765
GBann <GTBannon@protonmail.com>
2023-08-27 23:10:57 -0600

    fundamenals2

---
+++ Week1/Fundamentals2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Three basic arrays
+int[] numArr = {0,1,2,3,4,5,6,7,8,9};
+string[] strArr = {"Tim","Martin","Nikki","Sara"};
+bool[] boolArr = new bool[10];
+for(int i=0;i<boolArr.Length;i++){
+    if (i%2==0){
+        boolArr[i] = true;
+    }else {
+        boolArr[i] = false;
+    }
+    Console.WriteLine(boolArr[i]);
+}
+
+// List of Flavors
+List<string> iceCream = new List<string>();
+iceCream.Add("Vanilla");
+iceCream.Add("Rocky Road");
+iceCream.Add("Mint");
+iceCream.Add("Peach");
+iceCream.Add("Cherry Garcia");
+
+Console.WriteLine(iceCream.Count);
+Console.WriteLine(iceCream[2]);
+iceCream.RemoveAt(2);
+Console.WriteLine(iceCream.Count);
+
+// User Dictionary
+Dictionary<string,string> people = new Dictionary<string,string>();
+for(int i=0;i<strArr.Length;i++){
+    people.Add(strArr[i],iceCream[i]);
+}
+foreach(KeyValuePair<string,string> person in people){
+    Console.WriteLine($"{person.Key} - {person.Value}");
+}
