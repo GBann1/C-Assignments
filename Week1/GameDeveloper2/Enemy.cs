bb1545e4cbde4e756fcc8639fc5fe5a8053473ef
GBann <GTBannon@protonmail.com>
2023-08-28 18:22:50 -0600

    correct init

---
+++ Week1/GameDeveloper2/Enemy.cs
@@ -0,0 +1,18 @@
+public class Enemy
+{
+    public string Name;
+    public int Health;
+    public List<Attack> AttackList;
+    public Enemy(string name)
+    {
+        Name = name;
+        Health = 100;
+        AttackList = new List<Attack>();
+    }
+
+    public Attack RandomAttack()
+    {
+        Random rand = new Random();
+        return AttackList[rand.Next(AttackList.Count)];
+    }
+}
