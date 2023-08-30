4383f3e152fdbb0ec8377b1bb09676e52b9635a1
GBann <GTBannon@protonmail.com>
2023-08-30 13:51:51 -0600

    init dojosurvey w/ validations

---
+++ Week1/DojoSurvey_Validations/Models/SurveyModel.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+namespace DojoSurvey_Model.Models;
+
+public class SurveyModel
+{
+    public string Name {get;set;}
+    public string Location {get;set;}
+    public string Language {get;set;}
+    public string Comment {get;set;}
+}
