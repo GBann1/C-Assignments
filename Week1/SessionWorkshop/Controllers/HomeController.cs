327e45062ec01f8487b31b74d909e43a2fd8ff13
GBann <GTBannon@protonmail.com>
2023-08-31 10:09:42 -0600

    inital Session thoughts

---
+++ Week1/SessionWorkshop/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc;
+using SessionWorkshop.Models;
+
+namespace SessionWorkshop.Controllers;
+
+public class HomeController : Controller
+{
+    // Show "Input name screen"
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    // Does the work & redirects to the dashboard
+    [HttpPost("Login")]
+    public IActionResult Login()
+    {
+        HttpContext.Session.SetString("Name", "Gordon");
+        return RedirectToAction("Dashboard");
+    }
+
+    // Does the submission work to mod the Number in session
+    [HttpGet("Submit")]
+    public IActionResult Submit()
+    {
+        // I don't know how to set a default and then overwrite it
+        HttpContext.Session.SetInt32("Number", (HttpContext.Session.GetInt32("Number")+"###HIDDENINPUTVALUE###"));
+        return RedirectToAction("Dashboard");
+    }
+
+    // Renders the dashboard view
+    [HttpGet("Dashboard")]
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    // Clears session and redirects to login page
+    [HttpGet("Logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+}
