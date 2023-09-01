451379bc8fbd4f734a06fc263ce8531d8d70b925
GBann <GTBannon@protonmail.com>
2023-09-01 13:02:58 -0600

    minor null capture

--- Week1/SessionWorkshop/Controllers/HomeController.cs
+++ Week1/SessionWorkshop/Controllers/HomeController.cs
@@ -14,9 +14,11 @@ public class HomeController : Controller
 
     // Does the work & redirects to the dashboard
     [HttpPost("Login")]
-    public IActionResult Login(string name)
+    public IActionResult Login(string? name)
     {
-        
+        if (name == null){
+            return RedirectToAction("Index");
+        }
         HttpContext.Session.SetString("Name", name);
         // sets default number
         HttpContext.Session.SetInt32("Number", 22);
@@ -31,16 +33,18 @@ public class HomeController : Controller
         // Add the value from the Hidden Input to the value in session
         if (submission == "x2"){
             int newNum = (int)(num *2);
+            HttpContext.Session.SetInt32("Number", newNum);
             
         }else if (int.TryParse(submission, out int val)){
             int newNum = (int)(num + val);
+            HttpContext.Session.SetInt32("Number", newNum);
 
         }else {
             Random rand = new Random();
             int randNum = rand.Next(1,11);
             int newNum = (int)(num + randNum);
+            HttpContext.Session.SetInt32("Number", newNum);
         }
-        HttpContext.Session.SetInt32("Number", newNum);
         
         return RedirectToAction("Dashboard");
     }
