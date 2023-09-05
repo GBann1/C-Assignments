70e75f06386be832c82495514a5e71fce3ef31e5
GBann <GTBannon@protonmail.com>
2023-09-05 15:39:08 -0600

    tbh, no idea if this is correct

--- Week2/FirstConnection/Controllers/HomeController.cs
+++ Week2/FirstConnection/Controllers/HomeController.cs
@@ -1,31 +1,29 @@
-﻿using System.Diagnostics;
+﻿// Using statements
+using System.Diagnostics;
+using FirstConnection.Models;
 using Microsoft.AspNetCore.Mvc;
 using FirstConnection.Models;
-
-namespace FirstConnection.Controllers;
-
+namespace FirstConnection.Controllers;    
 public class HomeController : Controller
-{
+{    
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
+    // Add a private variable of type MyContext (or whatever you named your context file)
+    private MyContext _context;         
+    // Here we can "inject" our context service into the constructor 
+    // The "logger" was something that was already in our code, we're just adding around it   
+    public HomeController(ILogger<HomeController> logger, MyContext context)    
+    {        
         _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        // When our HomeController is instantiated, it will fill in _context with context
+        // Remember that when context is initialized, it brings in everything we need from DbContext
+        // which comes from Entity Framework Core
+        _context = context;    
+    }         
+    [HttpGet("")]    
+    public IActionResult Index()    
+    {     
+        // Now any time we want to access our database we use _context   
+        List<PetModel> AllPets = _context.PetModel.ToList();
+        return View();    
+    } 
 }
