eea4e595e5c38092619c08ff5f32b1e4a0f9a299
GBann <GTBannon@protonmail.com>
2023-09-12 20:19:45 -0600

    made files & completed chef model

---
+++ Week2/ChefsNDishes/Models/Chef.cs
@@ -0,0 +1,44 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace ChefsNDishes.Models;
+
+public class ChefsNDishes
+{
+    [Key]
+    public int ChefID {get; set;}
+
+    [Required]
+    [MinLength(2)]
+    public string FirstName {get; set;}
+
+    [Required]
+    [MinLength(2)]
+    public string LastName {get; set;}
+
+    [Required(ErrorMessage="Chef's birthday is required")]
+    [DateValidation(ErrorMessage="Chef must be over 18")]
+    public DateTime BirthDate {get; set;}
+
+    public DateTime CreatedAt {get;set;} = DateTime.Now;
+    public DateTime UpdatedAt {get;set;} = DateTime.Now;
+
+    public List<Dish> AllDishes {get; set;} = new List<DigitShapes>();
+}
+
+public class DateValidation : ValidationAttribute
+{
+    public override bool IsValid(object value)
+    {
+        // check value is formatted correctly & in past
+        if (value is DateTime BirthDate && BirthDate < DateTime.Now)
+        {
+            // find age
+            int age = DateTime.Now.Year - BirthDate.Year;
+            // check to see if they're over 18
+            return age >= 18;
+        }
+        return false;
+    }
+}
